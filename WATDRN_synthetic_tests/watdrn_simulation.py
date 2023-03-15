ebeeb7948f329fda924612fc087e92614820bab7
Ala <66693414+AlaBahrami@users.noreply.github.com>
2023-03-15 10:09:00 -0600

    Add WATDRN routine
    1) Include WATDRN stand-alone routines
    2) Add WATDRN simulations to MoC and Soulis' upscaling method

--- WATDRN_synthetic_tests/watdrn_simulation.py
+++ WATDRN_synthetic_tests/watdrn_simulation.py
@@ -7,6 +7,11 @@ Created on Thu Feb 16 23:39:11 2023
 See also: https://github.com/CH-Earth/laughTests/tree/master/lt4_wigmosta1994
 
 @author: Ala Bahrami
+
+last modified: 03/14/2023
+              1) include WATDRN stand-alone routines 
+              2) add WATDRN simulations to Moc and Soulis' upscaling method 
+    
 """
 #%% import modules 
 import matplotlib
@@ -55,6 +60,52 @@ kH = surfHydCond * tan_slope               #[m hour^-1]
 duration  = 20                                                  # time period [days]
 # ntsteps   = np.int32((duration/deltat) + 1)                     # number of time steps [-] 
 # time      = np.linspace(0,duration, ntsteps)
+
+#%% set variable and parameters required for WATDRN 
+# parameters required for WATDRN 
+# drainage density 
+# this is baed on Soulis et al (2000) and Mekonnen's draft 
+Dd1 = 1/(2*totalLength1)                           # [1/m]
+Dd2 = 1/(2*totalLength2)                           # [1/m]
+
+# obtain b coefficient  based on Clapp-Hornberger connectivity index  
+bij = (TOPMODEL_exp -3)/2                            # [-]
+
+# calculating xlambda explicitly by equating eq.(21) from Martyn's paper and eq.(24) from Mekonnen's draft 
+# NB: in MESH implementation, the xlambda decay factor is derived from xdrainh
+lambdda = - np.log(1+tan_slope**2)/(soilDepth)     #[1/m]
+lambdda = - lambdda                                # to be consistent with fortran code 
+
+# time step 
+delt = 1.0                                           # [hour]
+#---------------------------------------
+# initialize variables
+# NB: I assumed soil as one single layer 
+ILG = 1                                     # [-] number of tiles 
+IG  = 1                                     # [-] number of soil layers
+IL1 = 1                                     # [-] index of first tile
+IL2 = 1                                     # [-] index of last tile  
+IWF = 1                                     # [-] FLAG governing flat or slope CLASS. IWF = 0 activates flat CLASS
+ztop   = 0                                  # [m]
+delzw  = soilDepth                          # [m]
+ksat   = surfHydCond                        # [m hour^-1]
+xslope = tan_slope                          # [-]
+
+# Integration of saturated conductivity across the layer -> kl
+#xlambda        = -np.log(xdrainh[i])/h0
+ktop           = ksat * np.exp(lambdda*ztop)      #  [m hour^-1]
+
+# Important note : I commented calculation of kl using exav functions as it 
+# reduces the precision of calculation of grkeff
+#kl             = ktop * exav(lambdda*delzw)       #  [m hour^-1]
+kl             = ktop * np.exp(lambdda*delzw)       #  [m hour^-1]
+
+# calculate grkeff for two hillslope lengths 
+grkeff1         = kl*xslope*2.0*Dd1/(1+xslope**2)  # [hour^-1]
+grkeff2         = kl*xslope*2.0*Dd2/(1+xslope**2)  # [hour^-1]
+
+#thpor_avail[i] = np.max((thlmin[i,j],thpor_avail[i]))
+thpor_avail = porosity 
 
 #%% # Set font labels
 font = {'family' : 'Times New Roman',
@@ -330,6 +381,177 @@ def TOPMODEL_upscaling_outflow_saturated (qForce, kH, xL, zSoil, phi, nm, m, n):
     
     return zSave, qSave, tVec
 
+#%% WATDRN routine 
+# NB: as this experiment is run over one tile, the loop over tiles has been removed  
+def WATDRN(delzw,bcoef,thpora,grksat,grkeff,asat0,iwf, \
+     		ilg,il1,il2,wfk,delt):
+                   
+   # output variables 
+   # asat1(ilg)  ! bulk saturation at the end of the time step
+   # subflw(ilg) ! interflow amount during the time step (m)
+   # basflw(ilg) ! baseflow rate during the time step (m)
+   # satsfc(ilg) ! saturated fraction of the surface (0 to 1)
+    
+    if (not(np.all(iwf == wfk))):
+        exit
+    
+    # **********************************************************************
+    #      STEP 0: Initialize a few things before we start
+    #              - output variables
+    #              - functions of the input parameters
+    # **********************************************************************
+    # c      = np.zeros(ilg)            # Clapp-Hornberger connectivity index (c>1)
+    # cm1    = np.zeros(ilg)            # c-1
+    # c2m1   = np.zeros(ilg)            # 2*c-1
+    # asatc  = np.zeros(ilg)            # bulk saturation at the critical time tc
+    # asat00 = np.zeros(ilg)            # MIN(1,asat0) because we don't handle supersaturated soils
+    # tc     = np.zeros(ilg)            # critical time at which the seepage face becomes unsaturated
+    # ratiot = np.zeros(ilg)            # ratio tc/t (t0 or t1) if t>tc and t/tc if t<=tc
+    # satspf = np.ones(ilg, dtype=bool) # indicates if seepage face is saturated
+    #             					  # equivalent to knowing if t<=tc 
+    
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    
+    #c and c factors
+    c    = 2.*bcoef+3.
+    cm1  = c-1.
+    c2m1 = 2.*c-1.
+    
+    # bulk saturation at critical time
+    #(just before the seepage face becomes unsaturated)
+    asatc = 1.-1./c
+
+    # layer average saturation asat0 may be greater than 1
+    #e.g. frost heave but it is not possible for wat_drain
+    asat00 = np.min((1., asat0))
+    #assess if seepage face is saturated at initial time
+    satspf = asat00 >= asatc
+        
+    # **********************************************************************
+    #      STEP 1: Find theoretical time t0 elapsed since element was last
+    #              saturated and estimate baseflow rate at initial time
+    #              Also estimate fraction of surface that is saturated
+    # ********************************************************************** 
+    # tc = np.zeros(ilg)     # critical time at which the seepage face becomes unsaturated
+    # ratiot = np.zeros(ilg) # ratio tc/t (t0 or t1) if t>tc and t/tc if t<=tc
+    # basflw = np.zeros(ilg) # baseflow rate during the time step (m)
+    # satsfc = np.zeros(ilg) # saturated fraction of the surface (0 to 1)
+    
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+        # determine time at which seepage face becomes unsaturated
+    tc = thpora/(c*grkeff)
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    #find theoretical start of recession (t0) from bulk saturation
+    # and at the same time estimate baseflow based on rate at t0
+    if (satspf):
+      #saturated seepage face at initial time:
+      # compute t0/tc
+      ratiot = c*(1.-asat00)
+      # normalized baseflow rate
+      basflw = 1.-c*c/c2m1*(1.-asat00)
+      #the fraction of the surface that is saturated at t0
+      #varies linearly with t0/tc
+      satsfc= 1.-ratiot
+    else:
+      #unsaturated seepage face at initial time:
+      #calculate tc/t0 instead of t0 to avoid overflow 
+      ratiot = (asat00/asatc)**cm1
+      #normalized baseflow rate
+      basflw = cm1/c2m1*ratiot*asat00/asatc 
+      # the fraction of the surface that is saturated at t0 is zero
+      satsfc = 0.
+            
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    #Compute baseflow in m from normalized baseflow rate
+    basflw = grksat*basflw*delt
+        
+    # **********************************************************************
+    #      STEP 2: Find theoretical time t1 at the end of the time step
+    # **********************************************************************
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    if (satspf):
+        # Assess if seepage face will still be saturated at the
+        #  end of the time step
+        satspf = tc * ratiot + delt <= tc
+        if (satspf):
+            # Seepage face still saturated, compute t1/tc from t0/tc
+            ratiot= (tc*ratiot+delt)/tc
+        else:
+            # Seepage face not saturated anymore, compute tc/t1
+            ratiot = tc/(tc*ratiot+delt)
+    else:
+        # If seepage face was not saturated initially, we compute
+        # tc/t1=tc/(t0+delt) from tc/t0
+        ratiot = tc*ratiot/(tc+delt*ratiot)
+            
+    # **********************************************************************
+    #      STEP 3: Obtain bulk saturation at the end of the time step
+    #              and interflow amount
+    # **********************************************************************  
+    # asat1  = np.zeros(ilg)
+    # subflw = np.zeros(ilg) 
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    if (satspf):
+       # saturated seepage face at the end of the time step
+       asat1 = 1.-ratiot/c
+    else:
+       # unsaturated seepage face at the end of the time step
+       asat1= asatc*ratiot**(1./cm1)
+    
+    # for i in range(il1-1,il2):
+    #     # cycle if not using watrof or latflow
+    #     if (iwf[i] != wfk or asat0[i] == 0.0):
+    #         continue
+    # Sanity check: bulk saturation should not increase with time
+    asat1 = np.min((asat00,asat1))
+    # Obtain interflow from the difference in bulk saturation
+    subflw = (asat00-asat1)*thpora*delzw
+    
+    return asat1,subflw,basflw,satsfc
+
+#%% exav
+#**********************************************************************
+# 
+#      function exav(x)
+#      finds average of an exponential function exp(-x)
+#      over the interval [0,x], which is (1-exp(-x))/x
+#      deals with limit values of 1-x/2 when x->0 and 1/x when x->inf
+# 
+# **********************************************************************
+def exav(x):
+    # initialize variables 
+    exphuge  = 1.0e+9
+    expbig   = 1.0e+8
+    expsmall = 1.0e-8
+    
+    if (x > exphuge):
+        exav = 0.0
+    elif (x > expbig):
+        exav = 1.0/x  
+    elif (x > expsmall):
+        exav = (1.0-np.exp(-x))/x
+    else: 
+        exav = 1.0-x/2.0
+    return exav
+
 #%% Purpose is to find out how Xl and Hl during the rising of hydrograph
 # qKin1, qFall1, texit1, xL, hL  = kinematic_runoff(surfHydCond, soilDepth, TOPMODEL_exp, porosity, hillWidth * totalLength1, 
 #                                          hillWidth, totalLength1, hill_dist1,  precip)
@@ -478,6 +700,7 @@ Sc = 1 - 1/TOPMODEL_exp
 
 qx_qs = fractional_outflow (S, Sc , TOPMODEL_exp)
 
+#----------------------------------------------------------------------------
 # calling kinematic_runoff based on relative storage for rising and falling limb 
 # condition 1
 qKin1, qFall1, tExit1=  kinematic_runoff_RS(kH, soilDepth, TOPMODEL_exp, porosity, hillWidth * totalLength1, 
@@ -492,6 +715,43 @@ S_Soulis1, qx_soulis1, time_sim = Soulis_upscaling_outflow (precip, kH, totalLen
 
 S_Soulis2, qx_soulis2, time_sim = Soulis_upscaling_outflow (precip, kH, totalLength2, Sc, soilDepth, porosity, TOPMODEL_exp)
 
+#---------------------------------------
+# calling WATDRN routine  
+# variables related to WATDRN routine 
+p = len(S_Soulis1)
+
+asat_t1  = np.zeros(p) 
+subflw1  = np.zeros(p) 
+basflw1  = np.zeros(p)
+satfc1   = np.zeros(p)
+
+asat_t2  = np.zeros(p) 
+subflw2  = np.zeros(p) 
+basflw2  = np.zeros(p)
+satfc2   = np.zeros(p)
+
+# compute interflow from the layer (subflowj). Baseflow from the layer (basflwj) is 
+# also computed but is not used at present.
+
+# loop over entire time window of simulation for two hillslope lengths 
+for i in range(p):
+    # hillslope with length of totalLength1
+    [asat_t1[i], subflw1[i], basflw1[i], satfc1[i]] = WATDRN (delzw,bij,thpor_avail,ksat,grkeff1,S_Soulis1[i], IWF,\
+                                                             ILG,IL1,IL2,1,delt)
+    
+    # NB: in the source code of WATDRN the lateral flow is not devided by time step
+    subflw1[i] = MM_PER_M * subflw1[i]/delt                # convert to [mm hour^-1]
+
+    
+    # hillslope with length of totalLength2    
+    [asat_t2[i], subflw2[i],basflw2[i],satfc2[i]] = WATDRN (delzw,bij,thpor_avail,ksat,grkeff2,S_Soulis2[i],IWF,\
+                                                            ILG,IL1,IL2,1,delt)
+    
+    subflw2[i] = MM_PER_M * subflw2[i]/delt               # convert to [mm hour^-1]
+
+
+#---------------------------------------
+# plot results 
 # to be in same range of MoC
 time_sim = (time_sim-1)/len(precip)*duration
 
@@ -516,6 +776,8 @@ axs[1].plot(tExit1/24, MM_PER_M * qFall1, '#00007fe6')
 
 axs[1].plot(time_sim, MM_PER_M * qx_soulis1, '#47b1e2', label='Soulis upscaling method')
 
+axs[1].plot(time_sim, subflw1, '#ff0000', label='WATDRN')
+
 axs[1].text(14.8, 1.05, '$X_{L}$ = 50 m')
 axs[1].legend(fontsize = 14, loc = 'upper left',frameon=False)
 
@@ -523,6 +785,8 @@ axs[2].plot(time_rise, MM_PER_M * qKin2, '#00007fe6', label='Method of character
 axs[2].plot(tExit2/24, MM_PER_M * qFall2, '#00007fe6') 
 
 axs[2].plot(time_sim, MM_PER_M * qx_soulis2, '#47b1e2', label='Soulis upscaling method')
+
+axs[2].plot(time_sim, subflw2, '#ff0000', label='WATDRN')
 
 axs[2].text(14.8, 1.05, '$X_{L}$ = 10 m')
 axs[2].legend(fontsize = 14, loc = 'upper left',frameon=False)
@@ -540,7 +804,7 @@ axs[2].set_xlim(0,duration)
 axs[2].set_ylim(0,2.5)
 axs[2].grid(alpha=0.5)
 
-plt.savefig(outdir+'Figure5.png', format='png', dpi=300)
+plt.savefig(outdir+'Figure5_WATDRN.png', format='png', dpi=300)
 plt.close()
 
 #%% a test simulation based on WATDRN  
